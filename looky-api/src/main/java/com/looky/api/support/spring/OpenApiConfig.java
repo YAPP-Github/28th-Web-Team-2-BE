8e1b2d08fa8777035303b54a44bad7448dce12e4
kcw9609 <kang960953@gmail.com>
2026-06-24 21:18:15 +0900

    fix: 스웨거 HTTPS 서버 설정

--- looky-api/src/main/java/com/looky/api/support/spring/OpenApiConfig.java
+++ looky-api/src/main/java/com/looky/api/support/spring/OpenApiConfig.java
@@ -2,7 +2,12 @@ package com.looky.api.support.spring;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(
@@ -13,4 +18,12 @@ import org.springframework.context.annotation.Configuration;
         )
 )
 public class OpenApiConfig {
+
+        @Bean
+        public OpenAPI openAPI() {
+                return new OpenAPI()
+                        .servers(List.of(
+                                new Server().url("https://api.looky.my")
+                        ));
+        }
 }
